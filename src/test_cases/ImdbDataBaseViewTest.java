9bff838d2536e51b3b7ba138bd2dcb856846ba66
Satyabrata4957 <36322878+Satyabrata4957@users.noreply.github.com>
2018-02-11 01:23:30 +0600

    Update ImdbDataBaseViewTest.java

--- src/test_cases/ImdbDataBaseViewTest.java
+++ src/test_cases/ImdbDataBaseViewTest.java
@@ -6,10 +6,12 @@ import generic_lib.Database;
 
 public class ImdbDataBaseViewTest
 {
+	Logger log = Logger.getLogger("ImdbDataBaseViewTest");
 	@Test(alwaysRun=true)
 	public void fetchFromImdbDataBase()
 	{
 		Database db=new Database();
 		db.getMovieTable();
+		log.info("Reading data from database");
 	}
 }
